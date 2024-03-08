5aaa5e49d9d2d99c6c384586c86251693e63dfa8
DavitNarimanidze <davitnarimanidze58@gmail.com>
2024-03-08 19:13:05 +0400

    sixth commit

--- Day_06/classwork/lesson6.py
+++ Day_06/classwork/lesson6.py
@@ -1,4 +1,4 @@
-##surname = input("Enter Your Surname: ")
+surname = input("Enter Your Surname: ")
 age = input("Enter Your Age: ")
 
-print("Inputs Are: ",name,surname,age)
+print("Inputs Are : ",surname,age)
---
+++ Day_10/classwork/classwork10.py
@@ -0,0 +1,34 @@
+print(range(7,22))
+print(range(0,11))
+print(range(10,21))
+
+
+
+for i in range(11):
+    print(i)
+
+
+for i in range(20):
+    print(i)
+
+
+for i in range(11):
+    print("dato")
+
+
+
+
+
+
+
+for i in range(31):
+    print(i )
+
+
+for i in range(11):
+    print(i + i )
+
+
+i = 0
+for i in range(11):
+    print(i + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 )
