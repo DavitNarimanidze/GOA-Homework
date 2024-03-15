263502a617e36ac27e40bc4bd6611fa75a06fe67
DavitNarimanidze <davitnarimanidze58@gmail.com>
2024-03-15 16:27:52 +0400

    twelfth commit

---
+++ Day_012/homework/homework12.py
@@ -0,0 +1,65 @@
+# budget = int(input("Enter Your Budget: "))
+
+# if budget >= 100:
+#     print("you can buy this product")
+# else:
+#     print("you cant buy this product")
+#დავალება  1
+
+
+# password = "dato"
+# incorrect = 5
+# incorrect_count = 0
+
+# while incorrect_count < incorrect:
+#     user = input("Enter Password: ")
+#     if user != password:
+#         incorrect_count += 1
+#         print("incorrect try again. ")
+#         if incorrect_count == incorrect:
+#             print("too many incorrect attempts. System Blocked! ")
+#             break
+#     else:
+#         print("acces granted.")
+#         break
+#დავალება 2
+
+
+
+# a = int(input("Enter the first number: "))
+
+# choice = input("Enter your choice: ")
+
+# b = int(input("Enter the second number: "))
+
+# if choice == '+':
+#     print(a + b)
+# elif choice == "-":
+#     print(a - b)
+# elif choice == "*":
+#     print(a * b)
+# elif choice == '/':
+#     print(a / b)
+# else:
+#     print("Error")
+#დავალება 6
+
+
+
+
+# age = int(input("Enter Your Age: "))
+# if age >= 18:
+#     print("You can participate in the elections")
+# else:
+#     print("you can not participate in the elections")
+#დავალება 8
+
+
+
+# while True:
+#     num = int(input("enter the num :"))
+#     if num % 6 == 0 :
+#         print (num)
+#     else:
+#         print ("Try Again")
+#davaleba 7
