ad0ade3e7f8561ea429078a458959881b4d915c6
DavitNarimanidze <davitnarimanidze58@gmail.com>
2024-12-22 00:42:06 +0400

    frontend mentor

--- group 11/Level 00/homework/homework0.py
+++ group 11/Level 00/homework/homework0.py
@@ -1,36 +1,119 @@
-from turtle import * 
+# from turtle import * 
 
-# lesson1 Homework: Paint House
+# # lesson1 Homework: Paint House
 
-# Step 1: draw a square
+# # Step 1: draw a square
 
-width(3)
-color("orange")
+# speed(0)
+# width(3)
+# color("orange")
+# forward(200)
+# left(90)
+
+
+# forward(200)
+# left(90) 
+
+
+# forward(200)
+# left(90) 
+
+
+# forward(200)
+# left(90) 
+# end_fill()
+
+# #End Of Square
+
+# #Step 2: Drawing a door
+
+# forward(70)
+# color("brown")
+# begin_fill()
+# left(90)
+# forward(120)  #height of the door
+# right(90)
+# forward(60)
+# right(90)
+# forward(120)
+# end_fill()
+
+# penup()
+# goto(200, 200)
+# pendown()
+
+# color("red")
+# begin_fill()
+# right(150)
+# forward(200)
+# left(120)
+# forward(200)
+# end_fill()
+# penup()
+# goto(20, 180)
+# pendown()
+# color("blue") 
+# begin_fill()
+# left(30)
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+# end_fill()
+# penup()
+# goto(180, 180)
+# pendown()
+# color('blue')
+# begin_fill()
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+# end_fill()
+
+# penup()
+# goto(15,180)
+
+
+
+
+# exitonclick()
+
+
+from turtle import *
+#we want to paint a house
+
+#step 1: draw a square
+
+speed(30)
+width(7)
+color("purple")
 forward(200)
 left(90)
 
+forward(200)
+left(90)
 
 forward(200)
-left(90) 
-
-
-forward(200)
-left(90) 
-
+left(90)
 
 forward(200)
-left(90) 
-end_fill()
+left(90)
+#end of square
 
-#End Of Square
-
-#Step 2: Drawing a door
+#drawing a door
 
 forward(70)
-color("brown")
+color("yellow")
 begin_fill()
 left(90)
-forward(120)  #height of the door
+forward(120) #height of the door
 right(90)
 forward(60)
 right(90)
@@ -47,33 +130,22 @@ forward(200)
 left(120)
 forward(200)
 end_fill()
+
+
+#drawing a window
+
 penup()
-goto(20, 180)
+goto(15,150)
 pendown()
-color("blue") 
-begin_fill()
 left(30)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-end_fill()
-penup()
-goto(180, 180)
-pendown()
-color('blue')
-begin_fill()
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-end_fill()
+forward(100)
+
+
+
+
+
+
+
 
 
 
